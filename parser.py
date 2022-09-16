df23b40ae0f4f762da1bd6211c70aeb9ddaea205
Xavi Mañá <xavier.manya@gmail.com>
2022-09-16 18:59:33 +0200

    add read-folders parser

--- parser.py
+++ parser.py
@@ -1,19 +1,27 @@
 """
 Entrypoint for the client application
 """
+
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
 
 def get_parser() -> ArgumentParser:
-    parse:ArgumentParser = ArgumentParser(description='Yamala Hurry')
+    """
+    Method to instantiate the parser
+    """
+    parse:ArgumentParser = ArgumentParser(description='Yamala Hurry', epilog='Thank for using Yamala Hurry')
     subparser = parse.add_subparsers(title='subcommands', description='available subcommands')
 
     parser_files = subparser.add_parser('read-files', help='')
     parser_files.add_argument('files', nargs='+')
     parser_files.add_argument('-d', '--destination', dest='destination', default=Path.cwd(), type=Path)
 
-    #parser_folder = subparser.add_parser(name='read-folder', help='')
+    parser_folder = subparser.add_parser(name='read-folders', help='')
+    parser_folder.add_argument('files', nargs='+')
+    parser_folder.add_argument('-d', '--destination', dest='destination', default=Path.cwd(), type=Path)
+    parser_folder.add_argument('-r', '--recursive', dest='recursive', default=False, action='store_true')
+
     return parse
 
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,3 +1,7 @@
+"""
+Test cases for the application entrypoint
+"""
+
 import pytest
 
 from yamalahurry.parser import get_parser
@@ -17,7 +21,6 @@ def create_parser():
 
 @pytest.fixture
 def monkey_factory(monkeypatch):
-
     def get_cwd():
         return Path('/mock/cwd')
 
@@ -45,19 +48,75 @@ def monkey_factory(monkeypatch):
                              (#Test 4
                                      ['read-files', 'file1', 'file2', '--destination', '/folder/'],
                                      {'files': ['file1', 'file2'], 'destination': Path('/folder/')}
+                             ),
+                             (#Test 5
+                                    ['read-folders', 'folder1'],
+                                    {'files': ['folder1'], 'destination': Path.cwd(), 'recursive': False}
+                             ),
+                             (#Test 6
+                                    ['read-folders', 'folder1', 'folder2'],
+                                    {'files': ['folder1', 'folder2'], 'destination': Path.cwd(), 'recursive': False}
+                             ),
+                             (#Test 7
+                                    ['read-folders', 'folder1', 'folder2', '-d', '/folder/'],
+                                    {
+                                        'files': ['folder1', 'folder2'],
+                                        'destination': Path('/folder/'),
+                                        'recursive': False
+                                    }
+                             ),
+                             (#Test 8
+                                     ['read-folders', 'folder1', 'folder2', '--destination', '/folder/'],
+                                     {
+                                         'files':['folder1', 'folder2'],
+                                         'destination':Path('/folder/'),
+                                         'recursive': False
+                                     }
+                             ),
+                             (#Test 9
+                                     ['read-folders', 'folder1', '-r'],
+                                     {'files':['folder1'], 'destination':Path.cwd(), 'recursive': True}
+                             ),
+                             (#Test 10
+                                     ['read-folders', 'folder1', '--recursive'],
+                                     {'files':['folder1'], 'destination':Path.cwd(), 'recursive': True}
+                             ),
+                             (#Test 11
+                                     ['read-folders', 'folder1', '-d', '/folder/', '-r'],
+                                     {
+                                         'files':['folder1'],
+                                         'destination':Path('/folder/'),
+                                         'recursive': True
+                                     }
+                             ),
+                             (#Test 12
+                                     ['read-folders', 'folder1', '-d', '/folder/', '--recursive'],
+                                     {
+                                         'files':['folder1'],
+                                         'destination':Path('/folder/'),
+                                         'recursive':True
+                                     }
                              )
-                         ], ids=['one_file-default_cwd',
-                                 'two_files-default_cwd',
-                                 'two_files-d',
-                                 'two_files-destination'
+                         ], ids=['read_files-one_file-default_cwd',
+                                 'read_files-two_files-default_cwd',
+                                 'read_files-two_files-d',
+                                 'read_files-two_files-destination',
+                                 'read_folders-one_file-default_cwd-default_r',
+                                 'read_folders-two_files-default_cwd-default_r',
+                                 'read_folders-two_files-d-default_r',
+                                 'read_folders-two_files-destination-default_r',
+                                 'read_folders-one_file-default_cwd-r',
+                                 'read_folders-one_file-default_cwd-recursive',
+                                 'read_folders-one_file-d-r',
+                                 'read_folders-one_file-d-recursive',
                                  ]
                          )
-def test_readfiles(create_parser, monkey_factory, arguments, expected):
+def test_parser(create_parser, monkey_factory, arguments, expected):
     monkey_factory(arguments)
     namespace: Namespace = create_parser.parse_args(sys.argv)
     assert vars(namespace) == expected
     assert isinstance(namespace.destination, Path)
     assert isinstance(namespace.files, List)
-
-def test_rea
+    if hasattr(namespace, 'recursive'):
+        assert isinstance(namespace.recursive, bool)
 
