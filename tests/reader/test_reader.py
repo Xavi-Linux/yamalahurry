1818e361c66cf87d155d45654d749987933fd5b6
Xavi Mañá <xavier.manya@gmail.com>
2022-11-09 22:47:47 +0100

    add tests for pyyaml reader

--- tests/reader/test_reader.py
+++ tests/reader/test_reader.py
@@ -5,16 +5,141 @@ Tests for the yaml parser
 import pytest
 
 from yamalahurry.yamala.reader import FileTypeError, PyYamlReader
+from typing import Iterable
+from textwrap import dedent
 
 
+# #### Fixtures
 @pytest.fixture
-def instantiate_reader():
+def instantiate_pyyaml_reader():
     return PyYamlReader()
 
 
+@pytest.fixture
+def build_temp_file_factory(tmp_path):
+    folder = tmp_path / 'test_files'
+    folder.mkdir()
+
+    def wrapper(text: str, file_name: str):
+        filepath = folder / file_name
+        filepath.write_text(text)
+
+        return filepath
+
+    return wrapper
+
+
 # #### Happy Path
-def test_call(instantiate_reader):
-    instantiate_reader.load('/tmp/dir/file.yaml')
+@pytest.mark.parametrize(('text', 'name', 'expected'),
+                         [
+                              (#Test 1
+                                """
+                                a: 1
+                                b: 2
+                                """,
+                                'test.yaml',
+                                [{'a': 1, 'b': 2}]
+                              ),
+                              (#Test 2
+                                """
+                                version: 1.0
+                                app: yamala
+                                users:
+                                    - charmander@poke.mon
+                                    - squirtle@poke.mon
+                                    - pikachu@poke.mon
+                                """,
+                                'test_2.yml',
+                                [
+                                    {
+                                        'version': 1.0,
+                                        'app': 'yamala',
+                                        'users': [
+                                                    'charmander@poke.mon',
+                                                    'squirtle@poke.mon',
+                                                    'pikachu@poke.mon'
+                                                 ]
+                                    }
+                                ]
+                              ),
+                              (#Test 3
+                                 dedent(
+                                     """
+                                     ---
+                                     a: 1
+                                     b: 2
+                                     ...
+                                     ---
+                                     a: 5
+                                     b: 6
+                                     ...
+                                     """
+                                 ),
+                                 "test_3.yaml",
+                                 [
+                                     {'a': 1, 'b': 2},
+                                     {'a': 5, 'b': 6}
+                                 ]
+                              ),
+                              (#Test 4
+                                dedent(
+                                    """
+                                    version: '2.1'
+                                    services:
+                                        redis:
+                                            image: 'redis:5.0.5'
+                                            command: redis-server --requirepass redispass
+                                    
+                                        postgres:
+                                            image: postgres:9.6
+                                    """
+                                ),
+                                'test_4.yml',
+                                [
+                                    {
+                                        'version': '2.1',
+                                        'services': {
+                                                        'redis': {
+                                                                    'image': 'redis:5.0.5',
+                                                                    'command': 'redis-server --requirepass redispass'
+                                                                 },
+                                                        'postgres': {
+                                                                        'image': 'postgres:9.6'
+                                                                    }
+                                                    }
+                                    }
+                                ]
+                              ),
+                              (#Test 5
+                                dedent(
+                                    """
+                                    a:
+                                        b:
+                                            c:
+                                                - 1
+                                                - 2
+                                                - 3  
+                                    """
+                                ),
+                                'test_5.yaml',
+                                [
+                                    {'a': {'b': {'c': [1, 2, 3]}}}
+                                ]
+                              )
+                         ], ids=[
+                                    'simple-case-1',
+                                    'simple-list-1',
+                                    'multi-docs-1',
+                                    'mini-docker-compose-1',
+                                    'prone-to-be-flattened-1'
+                                ]
+
+                         )
+def test_pyyaml_content_retrieval(instantiate_pyyaml_reader, build_temp_file_factory, text, name, expected):
+    path = build_temp_file_factory(text, name)
+    content = instantiate_pyyaml_reader.load(path)
+    assert isinstance(content, Iterable)
+    assert content == expected
 
 
 # #### Sad Path
@@ -22,11 +147,17 @@ def test_call(instantiate_reader):
                          [
                              (#Test 1
                                  '/home/path/file.xlsx', 'File extension must be .yaml or .yml'
+                             ),
+                             (#Test2
+                                 '/home/path/file.yeml', 'File extension must be .yaml or .yml'
                              )
-                         ], ids=['excel_file-str']
+                         ], ids=[
+                                    'excel_file-str',
+                                    'yeml_extension'
+                                ]
                          )
-def test_file_ext(instantiate_reader, filepath, output):
+def test_file_ext(instantiate_pyyaml_reader, filepath, output):
     with pytest.raises(FileTypeError) as exp:
-        instantiate_reader.load(filepath)
+        instantiate_pyyaml_reader.load(filepath)
 
     assert exp.value.args[0] == output
