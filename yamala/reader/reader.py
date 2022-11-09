cb69a306d457c5f2ab96706ccbb2b0152f13b646
Xavi Mañá <xavier.manya@gmail.com>
2022-11-09 22:47:18 +0100

    add pyyaml reader

--- yamala/reader/reader.py
+++ yamala/reader/reader.py
@@ -1,39 +1,59 @@
 """
-Yaml parser
+Yaml readers: interfaces + implementations
 """
-from typing import Dict, Union, TypeVar, Iterable, Iterator
+from typing import Dict, List, Union, TypeVar, Iterable, Generator
 from pathlib import Path
-from yaml import load_all
 import abc
+import yaml
 
 PathLikeObj = TypeVar('PathLikeObj', str, Path)
 
 
 class FileTypeError(Exception):
-
+    """
+    Instantiate this class to raise when the supplied files is not yaml
+    """
     def __init__(self):
         Exception.__init__(self, 'File extension must be .yaml or .yml')
 
 
 class AbstractReader(abc.ABC):
-
+    """
+    Abstract class to ingrain an interface in any future yaml reader
+    """
     def __init__(self, *args, **kwargs):
         pass
 
     @staticmethod
     @abc.abstractmethod
-    def load(self, filepath: PathLikeObj) -> Union[Iterable, Iterator]:
+    def load(self, filepath: PathLikeObj) -> Iterable:
         return NotImplemented
 
     @staticmethod
     def _validate_extension(filepath: PathLikeObj) -> None:
-        if not (filepath[-4:]=='.yml' or filepath[-5:]=='.yaml'):
+        filepath_str: str
+        if isinstance(filepath, Path):
+            filepath_str = filepath.suffix
+        else:
+            filepath_str = filepath
+
+        if not (filepath_str[-4:]=='.yml' or filepath_str[-5:]=='.yaml'):
             raise FileTypeError()
 
 
 class PyYamlReader(AbstractReader):
-
-    def load(self, filepath: PathLikeObj) -> Iterator:
+    """
+    Implement a reader using third-party library pyyaml
+    """
+    def load(self, filepath: PathLikeObj) -> List:
         self._validate_extension(filepath)
-        return {}
+        files: List = []
+        with open(filepath, 'r') as f:
+            content: Generator = yaml.safe_load_all(f)
+            for file in content:
+                files.append(file)
+
+        return files
+
+
 
