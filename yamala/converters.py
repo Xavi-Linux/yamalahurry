8e8a93881ab40da27dd05fa8851654ceab86f6c6
Xavi Mañá <xavier.manya@gmail.com>
2022-12-06 11:46:22 +0100

    implement abstract converter

--- yamala/converters.py
+++ yamala/converters.py
@@ -1,1 +1,36 @@
+"""
+Classes that transform outputs of a reader into inputs of a writer
+"""
+
+import abc
+from pathlib import Path
 from .reader import AbstractReader, PyYamlReader
+from .writer import AbstractWriter, OpenxlpyWriter
+
+
+class AbstractConverter(abc.ABC):
+    """
+    Abstract class to ingrain some interface in any implemented converter
+    """
+
+    @abc.abstractmethod
+    def __init__(self, *args, **kwargs):
+        self.reader: AbstractReader = AbstractReader()
+        self.writer: AbstractWriter = AbstractWriter(Path.cwd())
+
+    @abc.abstractmethod
+    def read(self):
+        pass
+
+    @abc.abstractmethod
+    def transform(self):
+        pass
+
+    @abc.abstractmethod
+    def write(self):
+        pass
+
+    def execute(self):
+        self.read()
+        self.transform()
+        self.write()
