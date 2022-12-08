495cf4597bf9265982d22b764c8b3f8c1d385ccf
Xavi Mañá <xavier.manya@gmail.com>
2022-12-08 18:02:37 +0100

    add file inspector

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -5,8 +5,30 @@ Tests for the converter class
 import pytest
 
 from pathlib import Path
-from typing import Dict, List
+from typing import Callable, Dict, List, Literal
 from yamalahurry.yamala.converters import PyYamlToOpenpyxlConverter
+
+
+@pytest.fixture
+def files_path_factory(tmp_path) -> Callable:
+    dummy_dir: Path = tmp_path / 'test'
+    dummy_dir.mkdir()
+
+    def wrapper(files: List[str], element_type: Literal['d', 'f']) -> List[str]:
+        fake_paths: List[Path] = []
+        for file in files:
+            element: Path = dummy_dir / file
+            if element_type == 'd':
+                element.mkdir()
+
+            elif element_type == 'f':
+                element.write_text('1')
+
+            fake_paths.append(element)
+
+        return list(map(lambda f: str(Path(f).resolve()), fake_paths))
+
+    return wrapper
 
 
 def test_converter_instantiation():
@@ -24,3 +46,48 @@ def test_converter_instantiation():
     assert hasattr(converter, 'files') and isinstance(converter.files, List)
     assert hasattr(converter, 'destination') and isinstance(converter.destination, Path)
     assert hasattr(converter, 'recursive') and isinstance(converter.recursive, bool)
+
+
+@pytest.mark.parametrize(
+    ('inputs', 'expected'),
+    [
+        (#Test 1
+            [['folder1'], 'd'],
+            False
+        ),
+        (#Test 2
+            [['file.yml'], 'f'],
+            True
+        ),
+        (#Test 3
+            [['folder2', 'folder3', 'folder4'], 'd'],
+            False
+        ),
+        (#Test 4
+            [['file1.yml', 'file2.yml', 'file3.yml'], 'f'],
+            True
+        )
+    ], ids=[
+        'one-folder-1',
+        'one-file-1',
+        'multiple-folders-1',
+        'multiple-files-1'
+    ]
+)
+def test_files_param_inspector(files_path_factory, inputs, expected):
+    """
+    Test whether the converter is able to identify if files argument contains
+    files or directories. Additionally, it tests if the converter is casting
+    folder paths to Path instances.
+    """
+    new_inputs: List = files_path_factory(*inputs)
+
+    constructor: Dict = {
+        'files': new_inputs,
+        'destination': Path('/folder/'),
+        'recursive': False
+    }
+    converter = PyYamlToOpenpyxlConverter(**constructor)
+
+    assert converter._are_files == expected
+    assert all(map(lambda f: isinstance(f, Path), converter._converted_files))
--- yamala/converters.py
+++ yamala/converters.py
@@ -4,7 +4,7 @@ Classes that transform outputs of a reader into inputs of a writer
 
 import abc
 from pathlib import Path
-from typing import List
+from typing import List, Union
 from .reader import AbstractReader, PyYamlReader
 from .writer import AbstractWriter, OpenxlpyWriter
 
@@ -20,15 +20,15 @@ class AbstractConverter(abc.ABC):
         self.writer: AbstractWriter = AbstractWriter(Path.cwd())
 
     @abc.abstractmethod
-    def read(self):
+    def read(self) -> None:
         pass
 
     @abc.abstractmethod
-    def transform(self):
+    def transform(self) -> None:
         pass
 
     @abc.abstractmethod
-    def write(self):
+    def write(self) -> None:
         pass
 
     def execute(self):
@@ -43,17 +43,41 @@ class PyYamlToOpenpyxlConverter(AbstractConverter):
     Openpyxl.
     """
     def __init__(self, files: List[str], destination: Path, recursive: bool):
-        self.files: List[str] = files
+        self._files: List[str] = files
+        self._are_files: bool = True
+        self._converted_files: List[Path] = []
+
+        self._inspect_files()
+
         self.destination: Path = destination
         self.recursive: bool = recursive
         self.reader = PyYamlReader()
         self.writer = OpenxlpyWriter(Path.cwd())
 
-    def read(self):
+    @property
+    def files(self):
+        return self._files
+
+    @files.setter
+    def files(self, values: List[str]):
+        self._converted_files = []
+        self._files = values
+        self._inspect_files()
+
+    def read(self) -> None:
         pass
 
-    def transform(self):
+    def transform(self) -> None:
         pass
 
-    def write(self):
+    def write(self) -> None:
         pass
+
+    def _inspect_files(self) -> None:
+        for file in self._files:
+            self._converted_files.append(Path(file))
+            if not self._converted_files[-1].exists():
+                pass
+
+        self._are_files = all(map(Path.is_file, self._converted_files))
+
